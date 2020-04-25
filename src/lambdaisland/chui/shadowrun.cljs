8edc4f642acd5cd3bbf8033304d29836a89b7cdb
Arne Brasseur <arne@arnebrasseur.net>
2020-04-25 10:57:37 +0200

    Inline styles so consumers do not need garden

--- src/lambdaisland/chui/shadowrun.cljs
+++ src/lambdaisland/chui/shadowrun.cljs
@@ -2,7 +2,8 @@
   "Runner namespace to be used with shadow-cljs's :browser-test target.
 
   Not a 1989 fantasy tabletop game."
-  (:require [lambdaisland.chui.runner :as runner]
+  (:require [goog.dom :as gdom]
+            [lambdaisland.chui.runner :as runner]
             [lambdaisland.chui.ui :as ui]
             [lambdaisland.chui.test-data :as test-data]
             [lambdaisland.glogi :as log]
@@ -16,7 +17,8 @@
    lambdaisland.chui.interceptor :error})
 
 (defn start []
-  (ui/render! (.getElementById js/document "app"))
+  ;; for dev, enable this to update the UI on hot reload
+  #_(ui/render! (.getElementById js/document "chui-container"))
   (test-data/capture-test-data!)
   (js/window.requestIdleCallback
    #(ui/run-tests)))
@@ -25,4 +27,8 @@
   (runner/terminate! done))
 
 (defn ^:export init []
+  (let [app (gdom/createElement "chui-container")]
+    (gdom/setProperties app #js {:id "chui-container"})
+    (gdom/append js/document.body app))
+  (ui/render! (.getElementById js/document "chui-container"))
   (start))
