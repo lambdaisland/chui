7bc9893ac88d55a6d74b35db2e379781032ca306
Arne Brasseur <arne@arnebrasseur.net>
2020-04-29 11:27:14 +0200

    Some common sense CIDER settings

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
-((nil . ((cider-clojure-cli-global-options     . "-A:dev:test")
+((nil . ((cider-clojure-cli-global-options     . "-A:dev:test:kaocha-chui")
          (cider-custom-cljs-repl-init-form     . "(user/cljs-repl)")
          (cider-default-cljs-repl              . custom)
          (cider-preferred-build-tool           . clojure-cli)
