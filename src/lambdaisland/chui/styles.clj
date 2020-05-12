b09c8ee69528085672c6dfe7367acdafe5de254d
Felipe Barros <felipebarros@protonmail.com>
2020-05-12 01:53:10 -0300

    [WIP] Mega commit
    Major cleanup. Mostly minor bug fixes and removal of unnecessary stuff plus
    improvements.
    
    Still need to fix column 3's widget and colored border indicator, column
    scrolling (bottom is cut), fix the Garden code for styling the progress
    bars (Garden's pseudoselector shenanigans) and set some fallbacks for webkit
    browsers. Also bring button colors back.

--- src/lambdaisland/chui/styles.clj
+++ src/lambdaisland/chui/styles.clj
@@ -1,11 +1,12 @@
 (ns lambdaisland.chui.styles
   (:require [garden.core :as garden]
             [garden.stylesheet :as stylesheet]
-            [garden.selectors :as selectors]))
+            [garden.selectors :as s]
+            [garden.color :as color]))
 
-(def fail-color  "orange")
+(def fail-color  "#eeea0b")
 (def error-color "crimson")
-(def pass-color  "#29908a")
+(def pass-color  "#ccc")
 
 ;; https://github.com/chriskempson/base16-tomorrow-scheme/blob/master/tomorrow.yaml
 (def tomorrow
@@ -28,7 +29,7 @@
 
 (selectors/defselector input)
 
-(def search-input (partial input (selectors/attr= "type" "search")))
+(def search-input (partial input (s/attr= "type" "search")))
 
 (def puget-color-styles
   [[:code
@@ -47,242 +48,221 @@
     [:.insertion {:color (tomorrow :green)}]
     [:.deletion {:color (tomorrow :red)}]]])
 
+;; (s/defpseudoelement -moz-progress-bar)
+;; (s/defpseudoelement -webkit-progress-bar)
+;; (s/defpseudoelement -webkit-progress-value)
+
 (def style
-  [[:body {:overflow :hidden}]
-   [:#chui
-    [:* {:box-sizing "border-box"}]]
-   [:html {:color "#333"
-           :font-family "sans-serif"
+  [[:* {:box-sizing :border-box}]
+
+   [:html {:font-family "sans-serif"
            :height "100vh"}]
+
    [:body {:margin 0
-           :height "100%"}]
-   [:#chui :#chui-container {:height "100%"}
-    [:> [:div {:height "100%"
-               :display :grid
-               :grid-template-rows "auto 1fr"
-               :grid-gap ".3rem"}]]]
-   [:.top-bar {:background-color (:blue tomorrow)
-               :color "#7cdfff"
-               :padding ".5rem"
+           :height "100vh"
+           :overflow :hidden}]
+
+   [:#app {:height "100vh"}]
+
+   [:#chui {:height "100vh"
+            :overflow :hidden}]
+
+   [:.top-bar {:margin ".2rem"
                :display :flex
-               :justify-content :space-between}
-    [:.button {:padding ".3rem .6rem"
-               :background-color :whitesmoke
-               :border-radius "2px"
-               }]
-    [:.general-toggles
-     [:button :label {:margin-right "1rem"}]
-     [:input {:margin-right ".5rem"}]]
-    [:.name {:color :white
-             :text-decoration :none
-             :font-size "1.5rem"
-             :padding-right ".3rem"}]]
-   [:.interface-controls {:display :flex}]
-   [:.card {:border "1px solid #eee"
-            :box-shadow "1px 1px 5px #eee"}]
-   [:.inner-card {:border "1px solid #eee"}]
-   [:ul {:padding ".2rem"
-         :list-style :none
-         :text-decoration :none
-         :line-height 1.5}]
-   [:li [:a {:text-decoration :none}]]
-   [:code {:font-size "1.1rem"}]
-   [:main
-    {:display :flex
-     :width "100%"
-     :overflow-x "auto"
-     :scroll-snap-type "x mandatory"
-     :scrollbar-width :none
-     :background-color :initial}
-    [:&.cols-2 [:>section {:width "calc(100vw / 3)"}]]
-    [:&.cols-3
-     [:>section {:flex 1}
-      [:&:last-child {:flex 2}]]]
-    [:&.cols-4
-     [:>section {:width "20vw"}
-      [:&:last-child {:width "40vw"}]]]
-    [:>section {:flex-shrink 0
-                :display :flex
-                :flex-direction :column
-                :padding ".5rem"
-                :overflow :auto}
-     [:&:hover {:background-color :snow}]]]
-   [:.namespaces {:background-color :inherit}]
+               :justify-content :space-between
+               :align-items :center}]
+
+   [:.button {:border 0}
+    [:&:hover {:cursor :pointer}]]
+
+   [:.general-toggles {:display :flex
+                       :align-items :center}
+    [:input :label {:margin-right ".5rem"
+                    :vertical-align :text-bottom}]]
+
+   [:.clear {:margin-right ".5rem"
+             :padding ".5rem"}]
+
+   [:.name {:text-decoration :none
+            :padding-right ".3rem"}]
+
+   [:.card {:background-color "#f5f7fa"
+            :border "1px solid #d4dde9"
+            :margin-bottom ".2rem"}]
+
+   [:.inner-card {:background-color "white"
+                  :border "1px solid #d4dde9"}]
+
+   [:main {:display :flex
+           :padding-left ".2rem"
+           :height "100%"
+           :overflow :hidden
+           :gap ".2rem"}]
+
+   [:section {:flex 1}]
+
+   [:.column {:height "100%"
+              :overflow :scroll}]
+
+   [:.last-column {:flex 2
+                   :overflow-y :scroll}]
+
    [:.fieldset {:border "1px solid black"
                 :margin-top ".3rem"
                 :margin-bottom ".3rem"}]
+
    [(search-input)
     {:padding ".5rem"
      :border "none"
      :width "100%"
-     :font-size "1.1rem"
      :line-height 1.5}]
-   [(search-input "::placeholder") {:color :gray}]
 
-   [:.selection-target
-    [:&:hover {:background-color "#ff8"}]
-    [:&.selected {:background-color "lightgoldenrodyellow"}]]
-   [:.history { :background-color :inherit}]
-   [:.section-header {:font-size "1.1rem"
-                      :font-weight "bold"
-                      :width "100%"
+   ;; [(search-input "::placeholder") {}]
+
+   [:.selection-target                  ;FIX
+    [:&:hover {:background-color :white}]
+    [:&.selected {:background-color :white}]]
+
+   [:.section-header {:font-weight :normal
+                      :font-size "1.1rem"
                       :margin 0}]
-   [:.test-info {:background-color :initial
-                 :padding ".5rem 1rem 1rem"
-                 :margin-bottom "1rem"}
-    [:.inner-card {:padding ".3rem .5rem"
-                   :margin ".5rem 0"}]
+
+   [:.test-info {:padding ".5rem .2rem"
+                 :margin-bottom ".2rem"}
+    [:.inner-card {:padding ".3rem .2rem"
+                   :margin ".2rem 0"}]
     [:.assertion {:position :relative
                   :overflow-y :auto}]
-    [:.context :.message {:margin-bottom ".3rem"}]
+    [:.context :.message {:margin-bottom ".2rem"}]
     [:.pass {:border-right (str "4px solid " pass-color)}]
     [:.fail {:border-right (str "4px solid " fail-color)}]
     [:.error {:border-right (str "4px solid " error-color)}]
     [:aside {:position :absolute
              :top 0
              :right 0
-             :font-weight :bold
              :font-variant-caps :all-small-caps
-             :padding ".2rem .5rem"}]
+             :padding ".2rem .2rem"}]
     [:h4 {:margin 0
-          :font-variant-caps :all-small-caps
-          }]
-    [:.bottom-link {:width "100%"
-                    :display :block
-                    :text-align :right
-                    :margin-top "1rem"}]
-    ]
+          :font-weight :normal
+          :font-variant-caps :all-small-caps}]
+
+    [:.bottom-link {:text-decoration :none}]]
+
    [:.namespaces [:+ul {:padding-left "1.5rem"
                         :line-height "1.7rem"}]]
    [:.toggle {:position :absolute
               :left "-100vw"}]
    [:.namespace-selector {:display :flex
                           :flex-direction :column
-                          :margin-top ".5rem"
-                          :line-height 1.125}]
-   [:.active {:font-weight :bold}]
+                          :margin-top ".5rem"}]
+   [:.active {}]
    [:.search-bar {:display :grid
-                  :background-color :whitesmoke
                   :grid-template-columns "4fr minmax(26%, 1fr)"
                   :grid-auto-flow :column
                   :position :sticky
                   :top 0}]
-   [:.button {:font-variant-caps :all-small-caps
-              :font-weight :bold
-              :background-color :inherit
-              :border :none
-              :font-size "1.1rem"}
-    [:&:hover {:color :white
-               :cursor :pointer}]]
-   [:.run-tests {:color :silver
-                 :line-height ".9"}
-    [:&:hover :&:active {:background-color :lightgreen}]
-    [:&:hover:disabled {:background-color :silver}]]
-   [:.stop-tests {:color :coral}
-    [:&:hover {:background-color :lightcoral}]]
-   [:.namespace-links
-    {:display :flex
-     :flex-wrap :wrap
-     :border-radius "2px"
-     :align-items :center
-     :justify-content :space-between}
-    [:* {}
-     [:&:selected {:background-color :fuchsia}]]
-    [:input {:display :none
-             :width :max-content}]
-    [:label {:padding ".50rem .5rem"}]
-    [:aside {:padding ".50rem .5rem"}
-     [:small {:font-style :italic
-              :color :darkgray
-              :white-space :nowrap}]]]
 
-   [:.run
-    {:margin-bottom "1rem"
-     :opacity 0.7}
-    [:&.active {:opacity 1}]
+   [:.run-tests {:line-height ".9"}
+    [:&:hover :&:active {}]
+    [:&:hover:disabled {}]]
+
+   [:.stop-tests {}
+    [:&:hover {}]]
+
+   [:.namespace-links {:display :flex
+                       :flex-wrap :wrap
+                       :align-items :center
+                       :justify-content :space-between
+                       :line-height 1.5}
+    [:* {}
+     [:&:selected {}]]
+    [:input {:display :none
+             :width :max-content}]]
+   [:.namespace-link {:display :inline-flex
+                      :width "100%"
+                      :justify-content :space-between
+                      :flex-wrap :wrap
+                      :align-items :baseline}
+    [:span [:line-height 1.2]]
+    [:small {:color :gray}]]
+
+   [:.run {:opacity 0.5}
+    [:&.active {:opacity 1
+                :background-color :white
+                :border "1px solid darkgray"}]
+    [:output {:margin-bottom ".2rem"}]
+    [:&:hover {:opacity 1}]
     [:p {:margin 0}]
-    [:.run-header {:padding ".5rem 1rem"
-                   :grid-column-start 1
-                   :background-color :initial ;fix
-                   :color "#333"              ;fix
-                   :border-radius :initial    ;fix
-                   :justify-content :initial  ;fix
-                   :grid-column-end 3
-                   :display :grid
-                   :grid-template-columns :subgrid}
-     [:p {:grid-column-start 1}]
-     [:small {:grid-column-start 2
-              :color :gray
-              :text-align :right}]]
-    [:footer {:padding ".5rem 1rem"
+    [:footer {:padding ".5rem .2rem"
               :grid-column "1 /span 2"
               :grid-row-start 3}]
-    [:progress {:grid-column "1 / span 2"
+    [:.test-results {:margin "0 .2rem"}]]
+
+   [:.progress {:grid-column "1 / span 2"
+                :background :whitesmoke
                 :width "100%"
-                :height "4px"
+                :height "1rem"
                 :margin-top ".5rem"
-                :margin-bottom ".5rem"}]]
-   [:.test-results {:grid-column "1 / span 2"
-                    :line-height "1.6rem"
-                    :text-align :justify
-                    :margin "0 1rem"
-                    :overflow :hidden
-                    :font-size "50%"}
-    [:.ns {;;:border "1px solid darkslategray"
-           :overflow-wrap :anywhere
-           :box-shadow "1px 1px 4px #999"}]
-    [:.var {:border-right (str "1px solid #ccc")}
+                :margin-bottom ".5rem"
+                :border :none}]
+   ;; [:.moz-progress-error {:background :whitesmoke}
+   ;;  [(&) (& -moz-progress-bar) {:background error-color}]]
+   ;; [:.webkit-progress-error {:background :whitesmoke}
+   ;;  [(& -webkit-progress-bar {:background :whitesmoke})]
+   ;;  [(& -webkit-progress-value {:background error-color})]]
+
+   [:.run-header {:padding ".5rem .2rem"}
+    [:p {:grid-column-start 1}]
+    [:small {:grid-column-start 2
+             :text-align :right}]]
+
+   [:.test-results {:background-color :white}
+    [:.ns {:overflow-wrap :anywhere}]
+    [:.var {:margin-right ".2rem"
+            :line-height 1.5
+            :margin-bottom ".2rem"}
      [:&:last-child {:border-style :none}]]
-    [:output {:display :inline-box
-              :width "1em"}
+    [:output {:display :inline-block}
      [:.pass {:background-color pass-color}]
      [:.fail {:background-color fail-color}]
      [:.error {:background-color error-color}]]]
-   [:.ns-run
-    {:padding ".5rem 1rem 1rem"
-     :margin-bottom "1rem"
-     :font-family :sans-serif}
-    [:.ns-run--header
-     {:background-color :initial
-      :color :inherit
-      :display :inherit
-      :margin-bottom ".5rem"}
+
+   [:.ns-run {:padding ".5rem .2rem .5rem"
+              :margin-bottom ".2rem"
+              :font-family :sans-serif}
+
+    [:.ns-run--header {:display :inherit
+                       :margin-bottom ".5rem"}
      [:h2 {:font-weight :normal
-           :margin-bottom ".2rem"
-           :font-size "1.1rem"}]
-     [:.filename {:color :darkslategray
-                  :font-size ".8rem"
-                  :font-family :monospace}]]
+           :font-size "1rem"
+           :margin-bottom ".2rem"}]
+     [:.filename {:font-family :monospace
+                  :font-size ".8rem"}]]
+
     [:>div {:display :flex
             :flex-direction :column
-            :gap ".5rem"}]
+            :gap ".2rem"}]
+
     [:.ns-run--result {:flex-grow 1 :text-align :right}]
+
     [:.var-name-result {:display :flex
                         :flex-wrap :wrap}]
-    [:.ns-run-var
-     {:padding-left ".2rem"}
-     [:.test-results {:margin "-1px 0 0 0"}]
-     [:header
-      {:background-color :initial
-       :color :inherit
-       :border-radius :unset
-       :line-height 1.5
-       :display :flex}
+
+    [:.ns-run-var {:padding-left ".2rem"}
+     [:header {:border-radius :unset
+               :line-height 1.5
+               :display :flex}
       [:h3 {:font-weight :normal
             :font-size "1rem"
             :padding "0 1rem 0 0"}]
       [:p {:padding-right ".4rem"}]]
      [:h4 {:font-weight :normal
-           :font-size ".8rem"
            :padding-right ".2rem"}]]
-    [:.fail {:border-right (str "4px solid "  fail-color)}]
-    [:.error {:border-right (str "4px solid " error-color)}]
-    [:.pass {:border-right (str "4px solid "  pass-color)}]
+
     [:h2 :h3 :h4 :p {:margin 0}]
     [:code {:font-family :monospace
             :padding ".2rem"}]
-    [:.actual {:color :red
-               :font-weight :bold}]]])
+    [:.actual {}]]])
 
 (defmacro inline []
   (garden/css
