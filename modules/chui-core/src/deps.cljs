561de4f889a11d610c4276a38b98ddfe2c4b904e
Arne Brasseur <arne@arnebrasseur.net>
2020-08-17 15:46:57 +0200

    Declare npm-deps on chui-core, since multiple modules use them

---
+++ modules/chui-core/src/deps.cljs
@@ -0,0 +1,3 @@
+{:npm-deps {"stacktrace-js" "2.0.2"
+            "stack-trace" "0.0.10"
+            "platform" "1.3.5"}}
--- modules/chui-ui/src/deps.cljs
+++
@@ -1,2 +0,0 @@
-{:npm-deps {"platform" "1.3.5"
-            "stacktrace-js" "^2.0.2"}}
